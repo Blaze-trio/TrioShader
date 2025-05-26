a5124e2e4245d35e5ecb6e9dcda8d43ec30a53a1
Blaze-trio <64141242+Blaze-trio@users.noreply.github.com>
2025-05-26 17:39:42 +0300

    Refactor shaders: integrate basic vertex shader into gbuffer shaders for improved structure

--- shaders/basic_frag.glsl
+++ shaders/basic_frag.glsl
@@ -1,12 +1,16 @@
 #version 460
 uniform sampler2D gtexture;
+uniform sampler2D lightmap;
 /* DRAWBUFFERS:0*/
 layout(location = 0) out vec4 outColor0;
 in vec2 textCoord;
 in vec3 foliageColor;
+flat in ivec2 lightMapCoords;
+
 void main(){
+    vec3 lightColor = texture(lightmap, vec2(lightMapCoords) / 256.0).rgb;
     vec4 outputColorData = texture(gtexture,textCoord);
-    vec3 outputColor = outputColorData.rgb * foliageColor;
+    vec3 outputColor = outputColorData.rgb * foliageColor * lightColor;
     float transparency = outputColorData.a;
     if(transparency<.1){
         discard;
---
+++ shaders/basic_vert.glsl
@@ -0,0 +1,30 @@
+#version 460
+//attributes
+in vec3 vaPosition;
+in vec2 vaUV0;
+in vec4 vaColor; 
+in uvec2 vaUV2; // Changed from ivec2 to uvec2
+
+uniform vec3 chunkOffset; // offset for chunk position
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform vec3 cameraPosition;
+uniform mat4 gbufferModelViewInverse; // model view matrix for the GBuffer
+
+out vec2 textCoord;// the texture coordinate to be passed to the fragment shader
+out vec3 foliageColor; 
+flat out ivec2 lightMapCoords;
+
+void main(){
+    textCoord = vaUV0;
+    foliageColor = vaColor.rgb; // pass the color to the fragment shader
+    lightMapCoords = ivec2(vaUV2); // Explicit cast from uvec2 to ivec2
+    vec3 worldSpacePosition = cameraPosition + (gbufferModelViewInverse * modelViewMatrix * vec4(vaPosition+chunkOffset,1)).xyz;
+    float distanceFromCamera = distance(worldSpacePosition,cameraPosition);
+    
+    // Apply curvature by moving vertices downward based on distance
+    vec3 curvedPosition = vaPosition + chunkOffset;
+    curvedPosition.y -= 0.2 * distanceFromCamera; // Apply downward curvature
+    
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(curvedPosition, 1);
+}
